62e80b664919e6a32307f449a8e6d93d76709d30
Martin Juillard <martin.juillardfontanille@ynov.com>
2022-04-25 10:53:45 +0200

    ex 17

---
+++ is.js
@@ -0,0 +1,13 @@
+const is = {}
+
+is.num = 0
+is.nan = NaN
+is.str = "is a string"
+is.bool = true
+is.undef = undefined
+is.def = 1
+is.arr = []
+is.obj = {}
+is.fun = function () { }
+is.truthy = 1
+is.falsy = -1
