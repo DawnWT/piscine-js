9d88b447803b735e65b6ff6d26207771075f0152
Martin Juillard <martin.juillardfontanille@ynov.com>
2022-04-25 14:39:12 +0200

    fix

--- block-chain.js
+++ block-chain.js
@@ -1,12 +1,14 @@
 const defaultChain = { index: 0, hash: '0' }
 
 const blockChain = function (data, prev) {
+  // console.log(data, prev);
   if (!prev) prev = defaultChain
   const f = this
+  // console.log(data, prev);
 
   const ret = {
     index: ++prev.index,
-    hash: `${index}${prev.hash}${JSON.stringify(data)}`,
+    hash: `${this.index}${prev.hash}${JSON.stringify(data)}`,
     data: data,
     prev: prev
   }
