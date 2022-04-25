772df039c2b7fd1995baf8fc75b6518436eebfab
Martin Juillard <martin.juillardfontanille@ynov.com>
2022-04-25 10:11:37 +0200

    fix

--- change.js
+++ change.js
@@ -1,3 +1,11 @@
 Object.prototype.get = (a) => {
-  return this[a]
+  return this[a] || undefined
+}
+
+Object.prototype.get = (a, b) => {
+  if (!this[a]) {
+    return undefined
+  }
+  this[a] = b
+  return b
 }
