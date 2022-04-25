0f7b9d0f72b8cc457b5f14e313dcdbc0113867d0
Martin Juillard <martin.juillardfontanille@ynov.com>
2022-04-25 14:32:54 +0200

    ex 21

---
+++ block-chain.js
@@ -0,0 +1,15 @@
+const defaultChain = { index: 0, hash: '0' }
+
+const blockChain = function (data, prev) {
+  if (!prev) prev = defaultChain
+  const f = this
+
+  const ret = {
+    index: prev.index++,
+    hash: `${index}${prev.hash}${JSON.stringify(data)}`,
+    data: data,
+    prev: prev,
+    chain: (data2) => f(data2, this)
+  }
+  return ret
+}
