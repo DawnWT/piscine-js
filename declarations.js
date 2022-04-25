179ac78f16cce06c1b1e3f0c8fca80000221c2b5
Martin Juillard <martin.juillardfontanille@ynov.com>
2022-04-25 09:46:32 +0200

    ex 3

---
+++ declarations.js
@@ -0,0 +1,16 @@
+const escapestr = "\`\\\"\/\'"
+const arr = [4, '2']
+const obj = {
+  str: "",
+  num: 0,
+  bool: true,
+  undef: undefined
+}
+const nested = {
+  arr: [4, undefined, '2'],
+  obj: {
+    str: "",
+    num: 0,
+    bool: true
+  }
+}
