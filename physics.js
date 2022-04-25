be152b1b7ef50cd4c57060947ca90ec92beeae3f
Martin Juillard <martin.juillardfontanille@ynov.com>
2022-04-25 12:02:24 +0200

    fix

--- physics.js
+++ physics.js
@@ -2,6 +2,6 @@ const getAcceleration = (a) => {
   if (!a) return 'impossible'
   if (a.f && a.m) return a.f / a.m
   if (a.Δv && a.Δt) return a.Δv / a.Δt
-  if (a.d && a.t) return (2 * a.d) / (a.t ^ 2)
+  if (a.d && a.t) return (2 * a.d) / (a.t * a.t)
   return 'impossible'
 }
