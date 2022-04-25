1b8c0a35ddf05082085d0d9f716b720d15b39c6a
Martin Juillard <martin.juillardfontanille@ynov.com>
2022-04-25 11:24:40 +0200

    fix

--- is.js
+++ is.js
@@ -6,7 +6,7 @@ is.str = (a) => typeof a === 'string'
 is.bool = (a) => typeof a === 'boolean'
 is.undef = (a) => typeof a === 'undefined'
 is.def = (a) => typeof a !== 'undefined'
-is.arr = (a) => typeof a === 'object'
+is.arr = (a) => Array.isArray(a)
 is.obj = (a) => typeof a === 'object'
 is.fun = (a) => typeof a === 'function'
 is.truthy = (a) => Boolean(a)
