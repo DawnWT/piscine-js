ec951695c071eb7aba43e85aedb0215228266ba9
Martin Juillard <martin.juillardfontanille@ynov.com>
2022-04-26 13:35:05 +0200

    ex 26

---
+++ keep-cut.js
@@ -0,0 +1,6 @@
+const cutFirst = (a) => a.slice(2)
+const cutLast = (a) => a.slice(0, -2)
+const cutFirstLast = (a) => a.slice(2, -2)
+const keepFirst = (a) => a.slice(0, 2)
+const keepLast = (a) => a.slice(-2)
+const keepFirstLast = (a) => a.slice(-2, 2)
