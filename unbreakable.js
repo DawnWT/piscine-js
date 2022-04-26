28b304e1c892ad00d371f822c37df15b414f36f8
Martin Juillard <martin.juillardfontanille@ynov.com>
2022-04-26 14:43:10 +0200

    ex 28

---
+++ unbreakable.js
@@ -0,0 +1,33 @@
+const split = (a, b = null) => {
+  const ret = []
+  while (a.length > 0) {
+    const i = a.indexOf(b)
+    if (i !== -1) {
+      if (b) {
+        ret.push(a.slice(0, i))
+        a = a.slice(i + 1)
+      } else {
+        ret.push(a.slice(0, 1))
+        a = a.slice(1)
+      }
+    } else {
+      ret.push(a)
+      a = ''
+    }
+  }
+  return ret
+}
+
+const join = (a, b = null) => {
+  let ret = ''
+  for (const item of a) {
+    if (b === null) {
+      ret += (item + ', ')
+    } else if (b === '') {
+      ret += item
+    } else {
+      ret += (item + b)
+    }
+  }
+  return ret
+}
