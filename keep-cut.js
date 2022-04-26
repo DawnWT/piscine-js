03b0a439165a97c871409dc385b82076ab1d8a94
Martin Juillard <martin.juillardfontanille@ynov.com>
2022-04-26 13:37:04 +0200

    fix

--- keep-cut.js
+++ keep-cut.js
@@ -3,4 +3,4 @@ const cutLast = (a) => a.slice(0, -2)
 const cutFirstLast = (a) => a.slice(2, -2)
 const keepFirst = (a) => a.slice(0, 2)
 const keepLast = (a) => a.slice(-2)
-const keepFirstLast = (a) => keepFirst(a) + keepLast(a)
+const keepFirstLast = (a) => a.length > 4 ? keepFirst(a) + keepLast(a) : a
