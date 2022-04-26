b4f320816b5fe1f7e92bdba395c7d7ce9282e6bf
Martin Juillard <martin.juillardfontanille@ynov.com>
2022-04-26 13:31:30 +0200

    fix

--- slicer.js
+++ slicer.js
@@ -1,7 +1,7 @@
 const slice = (arr, start = 0, end = arr.length) => {
   const ret = []
-  if (start < 0) start = arr.length - start
-  if (end < 0) end = arr.length - end
+  if (start < 0) start = arr.length + start
+  if (end < 0) end = arr.length + end
   for (let i = start; i < end; i++) ret.push(arr[i])
 
   if (typeof arr === 'string') return ret.join('')
