119e2eea594a5b193c080a803c2622afb7947d93
Martin Juillard <martin.juillardfontanille@ynov.com>
2022-04-26 17:03:03 +0200

    fix

--- find-expression.js
+++ find-expression.js
@@ -2,7 +2,10 @@ const findExpression = (nb, total = 1, str = '') => {
   if (total === nb) {
     return str
   } else {
-    findExpression(nb, total + 4, str += add4)
-    findExpression(nb, total * 2, str += mul2)
+    const res1 = findExpression(nb, total + 4, str += add4)
+    if (res1) return res1
+    const res2 = findExpression(nb, total * 2, str += mul2)
+    if (res2) return res2
+    return undefined
   }
 }
