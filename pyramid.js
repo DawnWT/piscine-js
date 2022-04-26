b1976a90702aa9cad0f14e37a2db5520417141ca
Martin Juillard <martin.juillardfontanille@ynov.com>
2022-04-26 16:45:47 +0200

    fix

--- pyramid.js
+++ pyramid.js
@@ -1,7 +1,7 @@
 const pyramid = (a, b, baseB = b, ret = '') => {
   if (b === 1) {
     const spacesCount = (baseB - 1) * a.length
-    ret = getSpaces(spacesCount) + a + getSpaces(spacesCount) + ret
+    ret = getSpaces(spacesCount) + a + ret
     // for (let i = 0; i < b; i++) {
     //   ret = a + ret
     // }
