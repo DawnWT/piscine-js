45c2c5a6c1d2efa9b868521b74ee067e26f39ae6
Martin Juillard <martin.juillardfontanille@ynov.com>
2022-05-02 15:40:47 +0200

    fix

--- letter-space-number.js
+++ letter-space-number.js
@@ -2,9 +2,12 @@ const check = /[a-zA-Z] [[:digit:]][^[:digit:]a-zA-Z]/g
 
 const letterSpaceNumber = (str) => {
   str += ' '
-  const ret = str.matchAll(check)
+  const res = str.matchAll(check)
+  const ret = []
 
-  ret.map(v => {
-    return [v[0], v[1], v[2]]
-  })
+  for (const a of res) {
+    ret.push(a[0][0], a[0][1], a[0][2])
+  }
+
+  return ret
 }
