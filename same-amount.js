ba12cb4907265d6480efb82327516e9b62a409fd
Martin Juillard <martin.juillardfontanille@ynov.com>
2022-05-02 16:17:55 +0200

    fix

--- same-amount.js
+++ same-amount.js
@@ -1,8 +1,21 @@
 const sameAmount = (a, b, c) => {
-  b.flags += 'g'
-  c.flags += 'g'
-  const d = a.matchAll(b)
-  const e = a.matchAll(c)
+
+  let d = a.match(b)
+  let e = a.match(c)
+  let dCount = 0
+  let eCount = 0
+  while (d !== null && e !== null) {
+
+    if (d) {
+      dCount += 1
+      d = a.match(b)
+    }
+    if (e) {
+
+      eCount += 1
+      e = a.match(c)
+    }
+  }
 
   if (d.length === e.length) return true
   return false
