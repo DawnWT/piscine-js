ab8bcc4656f218bfa26cae5f7222a16b57bafb4a
Martin Juillard <martin.juillardfontanille@ynov.com>
2022-05-03 14:48:42 +0200

    fix

--- get-some-time.js
+++ get-some-time.js
@@ -5,5 +5,5 @@ const firstDayWeek = (w, y) => {
   const valW = valY + ((w - 1) * 7 * 24 * 60 * 60 * 1000)
 
   const date = new Date(valW)
-  return `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`
+  return `${date.getDate() < 10 ? '0' + date.getDate() : date.getDate()}-${date.getMonth() < 9 ? '0' + (date.getMonth() + 1) : (date.getMonth() + 1)}-${date.getFullYear()}`
 }
