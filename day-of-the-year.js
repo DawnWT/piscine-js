9443e8f72d3e027801efb5a630871202b82d5855
Martin Juillard <martin.juillardfontanille@ynov.com>
2022-05-03 15:48:00 +0200

    fix

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -1,6 +1,6 @@
 const dayOfTheYear = (date) => {
   const y = date.getFullYear()
-  const dayY = new Date(y.toString())
+  const dayY = new Date(y < 10 ? '000' + y : (y < 100 ? '00' + y : (y < 1000 ? '0' + y : y)))
 
   const mili = date.valueOf() - dayY.valueOf()
 
