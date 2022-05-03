71d5fb1e36beb87aab9456fae7a4d091cceff829
Martin Juillard <martin.juillardfontanille@ynov.com>
2022-05-03 15:10:06 +0200

    ex 44

---
+++ rebecca-black.js
@@ -0,0 +1,23 @@
+const isFriday = (date) => {
+  if (date.getDay() === 5) return true
+  return false
+}
+
+const isWeekend = (date) => {
+  if (date.getDay() > 5) return true
+  return false
+}
+
+const isLeapYear = (date) => {
+  const y = date.getFullYear()
+  if ((y % 4 === 0 && y % 100 !== 0) || y % 400 === 0) return true
+  return false
+}
+
+const isLastDayOfMonth = (date) => {
+  date.setDate(day + 1)
+  const day = date.getDate()
+
+  if (date.getDate() === 1) return true
+  return false
+}
