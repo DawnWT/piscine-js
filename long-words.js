459da7cbc1fc5b8e3585f1857cd9f378664d6150
Martin Juillard <martin.juillardfontanille@ynov.com>
2022-05-09 14:36:57 +0200

    fix

--- long-words.js
+++ long-words.js
@@ -1,5 +1,5 @@
-const longWords = (arr) => arr.length === arr.filter(w => w.length > 4).length ? true : false
+const longWords = (arr) => arr.every(w => w.length > 4)
 
-const oneLongWord = (arr) => arr.filter(w => w.length > 9).length > 0 ? true : false
+const oneLongWord = (arr) => arr.some(w => w.length > 9)
 
-const noLongWords = (arr) => arr.filter(w => w.length > 6).length === 0 ? true : false
+const noLongWords = (arr) => !arr.some(w => w.length > 6)
