22b5e8011ced4ff554f561eeaa2703eb0e8c0315
Martin Juillard <martin.juillardfontanille@ynov.com>
2022-05-09 14:18:24 +0200

    fix

--- using-filter.js
+++ using-filter.js
@@ -4,6 +4,23 @@ const filterStartVowel = (arr) => arr.filter(w => w.match(/^[aeiou]/gi))
 
 const filter5Vowels = (arr) => arr.filter(w => w.match(/(.*[aeiou].*){5,}/gi))
 
-const filter1DistinctVowel = (arr) => arr.filter(w => w.match(/[aeiou]{1,}/gi))
+const filter1DistinctVowel = (arr) => arr.filter(w => {
+  const vowels = 'aeiouAEIOU'
+  let wordVowel = ''
+  // let keep = true
+  for (const char of w) {
+    if (wordVowel === '' && vowels.includes(char)) {
+      wordVowel = char
+      continue
+    }
+
+    if (!vowels.includes(char)) continue
+
+    if (wordVowel !== char) {
+      return false
+    }
+  }
+  return true
+})
 
 const multiFilter = (arr) => arr.filter(w => w.capital.length > 8 && w.name.match(/^[^aeiou]]/gi) && w.tag.match(/[aeiou]{1,}/gi) && w.region !== 'South')
