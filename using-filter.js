9a3c72576500d5d3916186ea5958d0eba01b577b
Martin Juillard <martin.juillardfontanille@ynov.com>
2022-05-09 14:06:51 +0200

    fix

--- using-filter.js
+++ using-filter.js
@@ -1,9 +1,9 @@
 const filterShortStateName = (arr) => arr.filter(w => w.length < 7)
 
-const filterStartVowel = (arr) => arr.filter(w => w.match(/^[aeiouAEIOU]/gi))
+const filterStartVowel = (arr) => arr.filter(w => w.match(/^[aeiou]/gi))
 
-const filter5Vowels = (arr) => arr.filter(w => w.match(/[aeiouAEIOU]{5,}/gi))
+const filter5Vowels = (arr) => arr.filter(w => w.match(/(.*[aeiou].*){5,}/gi))
 
-const filter1DistinctVowel = (arr) => arr.filter(w => w.match(/[aeiouAEIOU]{1,}/gi))
+const filter1DistinctVowel = (arr) => arr.filter(w => w.match(/[aeiou]{1,}/gi))
 
-const multiFilter = (arr) => arr.filter(w => w.capital.length > 8 && w.name.match(/^[^aeiouAEIOU]]/gi) && w.tag.match(/[aeiouAEIOU]{1,}/gi) && w.region !== 'South')
+const multiFilter = (arr) => arr.filter(w => w.capital.length > 8 && w.name.match(/^[^aeiou]]/gi) && w.tag.match(/[aeiou]{1,}/gi) && w.region !== 'South')
