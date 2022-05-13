80cfe31bd0db7f8251bdd471426a6e9ea4298d46
Martin Juillard <martin.juillardfontanille@ynov.com>
2022-05-13 18:24:21 +0200

    ex 61

---
+++ build-brick-and-break.js
@@ -0,0 +1,28 @@
+const build = (n) => {
+  let i = 0
+  const interval = setInterval(() => {
+    const brick = document.createElement('div')
+    brick.id = `brick-${i + 1}`
+
+    if (i % 3 === 1) brick.dataset.foundation = true
+
+    document.body.append(brick)
+
+    if (i === n - 1) clearInterval(interval)
+
+    i++
+  }, 100)
+}
+
+const repair = (...ids) => {
+  for (const id of ids) {
+    const brick = document.querySelector(`#brick-${id}`)
+    brick.setAttribute('repaired', id % 3 === 2 ? 'in progress' : true)
+  }
+}
+
+const destroy = () => {
+  const lastBrick = document.querySelector('div:last-child')
+
+  lastBrick.remove()
+}
