27facfd5f06ee791876e63ba6f44d5676b8fa7ee
Martin Juillard <martin.juillardfontanille@ynov.com>
2022-05-13 18:05:56 +0200

    fix

--- get-them-all.js
+++ get-them-all.js
@@ -1,15 +1,15 @@
-const getArchitects = () => {
+export const getArchitects = () => {
   return [document.querySelectorAll('a'), document.querySelectorAll(':not(a)')]
 }
 
-const getClassical = () => {
+export const getClassical = () => {
   return [document.querySelectorAll('a.classical'), document.querySelectorAll('a:not(.classical)')]
 }
 
-const getActive = () => {
+export const getActive = () => {
   return [document.querySelectorAll('a.classical.active'), document.querySelectorAll('a.classical:not(.active)')]
 }
 
-const getBonannoPisano = () => {
+export const getBonannoPisano = () => {
   return [document.querySelector('a#BonannoPisano'), document.querySelectorAll('a.classical.active:not(#BonannoPisano)')]
 }
