213bc23139aaeccdac325b7f9e8c62cd3525e9e5
Martin Juillard <martin.juillardfontanille@ynov.com>
2022-05-13 22:55:00 +0200

    fix

--- keycodes-symphony.js
+++ keycodes-symphony.js
@@ -1,5 +1,13 @@
 const authorized = ['a', 'z', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'q', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'w', 'x', 'c', 'v', 'b', 'n', 'Backspace', 'Escape']
 
+const randomNb = (n) => {
+  const max = 10 * (n - 1)
+  const nb = Math.floor(Math.random() * max)
+  let str = nb.toString()
+  for (let i = 0; i < n - 1; i++) if (nb < (10 * (i + 1))) str = `0${str}`
+  return str
+}
+
 const trueCompose = (e) => {
   if (!authorized.includes(e.key)) return
 
@@ -20,6 +28,7 @@ const trueCompose = (e) => {
   const div = document.createElement('div')
   div.classList.add('note')
   div.textContent = e.key
+  div.style.background = `#${randomNb(6)}`
 
   document.body.append(div)
 }
