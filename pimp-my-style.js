ec728e94bdcd8baad6c779d31cc855c6e14aebf2
Martin Juillard <martin.juillardfontanille@ynov.com>
2022-05-13 19:05:25 +0200

    fix

--- pimp-my-style.js
+++ pimp-my-style.js
@@ -1,22 +1,23 @@
 let i = 0
 let unpimp = false
+//const styles = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen"];
 
 export const pimp = () => {
   const btn = document.querySelector('.button')
 
   if (!unpimp) {
     btn.classList.add(styles[i])
-    i++
     if (i === styles.length - 1) {
       unpimp = true
       btn.classList.add('unpimp')
     }
+    i++
   } else {
     btn.classList.remove(styles[i])
-    i--
     if (i === 0) {
       unpimp = false
       btn.classList.remove('unpimp')
     }
+    i--
   }
 }
