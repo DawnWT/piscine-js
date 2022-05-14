68f9bfb24d0767a41eb7cdb03543852dda531552
Martin Juillard <martin.juillardfontanille@ynov.com>
2022-05-14 12:41:37 +0200

    fix

--- race.js
+++ race.js
@@ -3,9 +3,12 @@ const race = async (arr) => new Promise((res, rej) => {
 })
 
 const some = async (arr, n) => {
-  if (arr.length === 0 || n === 0) return new Promise(res => undefined)
+  const inedexes = []
   const ret = []
-  for (const func of arr) func.then(val => ret.push(val))
-
-  return ret.slice(0, n) || []
+  for (let i = 0; i < n; i++) {
+    ret.push(new Promise((res, rej) => {
+      for (const promise of arr.filter((_, i) => !inedexes.includes(i))) promise.then(res).catch(rej)
+    }))
+  }
+  return ret
 }
