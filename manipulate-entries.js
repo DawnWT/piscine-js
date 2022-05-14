197669e7a961b3a8a4e4ffed95bf27706b4b1442
Martin Juillard <martin.juillardfontanille@ynov.com>
2022-05-14 03:05:58 +0200

    fix

--- manipulate-entries.js
+++ manipulate-entries.js
@@ -42,5 +42,5 @@ const lowCarbs = (obj) => {
 }
 
 const cartTotal = (obj) => {
-  return mapEntries(obj, ([key, val]) => [key, mapValues(nutritionDB[key], (v) => (v * 10 * (val / 1000)))])
+  return mapEntries(obj, ([key, val]) => [key, mapValues(nutritionDB[key], (v) => Math.round((v * (val / 100)) * 100) / 100)])
 }
