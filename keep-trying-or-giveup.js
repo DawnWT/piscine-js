c286fe5417839856074ae77da397d01f9a813d66
Martin Juillard <martin.juillardfontanille@ynov.com>
2022-05-14 07:28:01 +0200

    fix

--- keep-trying-or-giveup.js
+++ keep-trying-or-giveup.js
@@ -1,11 +1,14 @@
 const retry = (count, cb) => async (...args) => {
   try {
-    return await cb(...args)
+    return cb(...args)
   }
   catch (e) {
-    if (count > 0) return await retry(count - 1, cb)(...args)
+    if (count > 0) return retry(count - 1, cb)(...args)
     else throw e
   }
 }
 
-// const timeout = async (delay, cb) =>await async (...args) => 
+const timeout = (delay, cb) => async (...args) => {
+  setTimeout(() => { throw new Error('timeout') }, delay)
+  return cb(...args)
+}
