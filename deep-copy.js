68bd215ef9c64f203db698e782701bfd06a878fb
Martin Juillard <martin.juillardfontanille@ynov.com>
2022-05-14 03:39:11 +0200

    fix

--- deep-copy.js
+++ deep-copy.js
@@ -1,16 +1,14 @@
-const deepCopy = (obj, copy = Array.isArray(obj) ? [] : {}, finish = false) => {
-  if (finish) {
-    return copy
-  }
-
+const deepCopy = (obj, copy = Array.isArray(obj) ? [] : {}, base = true) => {
   if (Array.isArray(obj)) {
     for (const val of obj) {
-      copy.push(deepCopy(val, copy, true))
+      copy.push(deepCopy(val, copy, false, false))
     }
+    if (base) return copy
   } else if (typeof obj === 'object') {
     for (const [key, val] of Object.entries(obj)) {
-      copy[key] = deepCopy(val, copy, true)
+      copy[key] = deepCopy(val, copy, false, false)
     }
+    if (base) return copy
   } else {
     return obj
   }
