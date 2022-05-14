1c56809dfe01736126fedc7145bccf011add2a5e
Martin Juillard <martin.juillardfontanille@ynov.com>
2022-05-14 04:12:58 +0200

    fix

--- replica.js
+++ replica.js
@@ -1,5 +1,6 @@
 const recuReplica = (obj1, obj2) => {
   if (typeof obj2 !== 'object' || Array.isArray(obj2) || obj2 instanceof RegExp) return obj2
+  if (typeof obj1 !== 'object' || Array.isArray(obj1) || obj1 instanceof RegExp) return obj2
 
   for (const [key, val] of Object.entries(obj2)) {
     obj1[key] = recuReplica(obj1[key], val)
