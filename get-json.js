a413797fcc10503b5275bf273910249a5d5aa771
Martin Juillard <martin.juillardfontanille@ynov.com>
2022-05-14 08:02:23 +0200

    ex 80

---
+++ get-json.js
@@ -0,0 +1,11 @@
+const getJson = async (path, ...params) => {
+  const url = new URL(path)
+  for (const [key, value] of Object.entries(params)) {
+    url.searchParams.append(key, value)
+  }
+  const urlFetch = await fetch(url)
+  const json = await urlFetch.json()
+
+  if (json.error) throw new Error(json.error)
+  return json.data
+}
