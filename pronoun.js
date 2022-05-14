9efdc802a182bc8b3646c5fed3346e212023b077
Martin Juillard <martin.juillardfontanille@ynov.com>
2022-05-14 04:22:21 +0200

    ex 77

---
+++ pronoun.js
@@ -0,0 +1,15 @@
+const pronouns = ['i', 'you', 'he', 'she', 'we', 'they']
+
+const pronoun = (str) => {
+  const ret = {}
+  const splitted = str.toLowerCase().split(' ')
+
+  for (const [i, word] of splitted.entries()) {
+    if (pronouns.includes(word)) {
+      if (ret[word] === undefined) ret[word] = { word: [], count: 0 }
+
+      ret[word].count += 1
+      if (i < splitted.length && !pronouns.includes(splitted[i + 1])) ret[word].word.push(word)
+    }
+  }
+}
