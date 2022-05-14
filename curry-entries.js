a056adf47fcad6925df5b1b7250484df6aa7dfdc
Martin Juillard <martin.juillardfontanille@ynov.com>
2022-05-14 03:16:12 +0200

    ex 74

---
+++ curry-entries.js
@@ -0,0 +1,41 @@
+const defaultCurry = (obj) => (obj2) => ({ ...obj, ...obj2 })
+
+const mapCurry = (func) => (obj) => {
+  const newObj = {}
+  for (const [key, val] of Object.entries(obj)) {
+    const [nk, nv] = func([key, val])
+    newObj[nk] = nv
+  }
+  return newObj
+}
+
+const reduceCurry = (func) => (obj, initialValue = '') => {
+  let acc = initialValue
+  for (const [key, val] of Object.entries(obj)) {
+    acc = func(acc, [key, val])
+  }
+  return acc
+}
+
+const filterCurry = (func) => (obj) => {
+  const newObj = {}
+  for (const [key, val] of Object.entries(obj)) {
+    if (func([key, val])) {
+      newObj[key] = val
+    }
+  }
+  return newObj
+}
+
+const reduceScore = (personnel, initialValue = 0) => {
+  const filtered = filterCurry(([, val]) => val.isForceUser === true)(personnel)
+  return reduceCurry((acc, [, val]) => acc + val.pilotingScore + val.shootingScore, initialValue)(filtered)
+}
+
+const filterForce = (personnel) => {
+  return filterCurry(([, val]) => val.isForceUser === true && val.shootingScore > 80)(personnel)
+}
+
+const mapAverage = (personnel) => {
+  return mapCurry(([key, val]) => [key, { ...val, averageScore: (val.pilotingScore + val.shootingScore) / 2 }])(personnel)
+}
