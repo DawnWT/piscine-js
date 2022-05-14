4f5f9d5db757141a3f9536b1da2ac15b366e6a93
Martin Juillard <martin.juillardfontanille@ynov.com>
2022-05-14 05:38:36 +0200

    fix

--- interpolation.js
+++ interpolation.js
@@ -1,7 +1,7 @@
 const interpolation = ({ step, start, end, callback, duration }) => {
   let i = 0
   const interval = setInterval(() => {
-    const point = [start + i * (end / step), start + (i + 1) * (duration / step)]
+    const point = [start + i * ((end - start) / step), (i + 1) * (duration / step)]
     callback(point)
 
     if (i++ === step - 1) clearInterval(interval)
