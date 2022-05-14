437645d3275407b524b41cfae2cfa715c0ad1201
Martin Juillard <martin.juillardfontanille@ynov.com>
2022-05-14 05:00:09 +0200

    ex 78

---
+++ interpolation.js
@@ -0,0 +1,9 @@
+const interpolation = ({ step, start, end, callback, duration }) => {
+  let i = 0
+  const interval = setInterval(() => {
+    const point = [start + i * (end / step), start + (i + 1) * (duration / step)]
+    callback(point)
+
+    if (i === step - 1) clearInterval(interval)
+  }, duration / step)
+}
