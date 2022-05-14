087de0e38ae7fb35d43c27614b55ca8d0a82f324
Martin Juillard <martin.juillardfontanille@ynov.com>
2022-05-14 12:36:16 +0200

    fix

--- race.js
+++ race.js
@@ -1,10 +1,6 @@
-const race = async (arr) => {
-  if (arr.length === 0) return new Promise(res => undefined)
-  const ret = []
-  for (const func of arr) func.then(val => ret.push(val))
-
-  return ret[0] || []
-}
+const race = async (arr) => new Promise((res, rej) => {
+  for (const promise of arr) promise.then(res).catch(rej)
+})
 
 const some = async (arr, n) => {
   if (arr.length === 0 || n === 0) return new Promise(res => undefined)
