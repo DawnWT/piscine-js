cbc9fd9476cd7f265f6c351e6517f32273a842f4
Martin Juillard <martin.juillardfontanille@ynov.com>
2022-05-14 02:50:01 +0200

    ex 73

---
+++ manipulate-entries.js
@@ -0,0 +1,38 @@
+const filterEntries = (obj, func) => {
+  const newObj = {}
+  for (const [key, val] of Object.entries(obj)) {
+    if (func([key, val])) {
+      newObj[key] = val
+    }
+  }
+  return newObj
+}
+
+const mapEntries = (obj, func) => {
+  const newObj = {}
+  for (const [key, val] of Object.entries(obj)) {
+    [nk, nv] = func([key, val])
+    newObj[nk] = nv
+  }
+  return newObj
+}
+
+const reduceEntries = (obj, func, initialValue = '') => {
+  let acc = initialValue
+  for (const [key, val] of Object.entries(obj)) {
+    acc = func(acc, [key, val])
+  }
+  return acc
+}
+
+const totalCalories = (obj) => {
+  return reduceEntries(obj, (acc, [key, val]) => acc + nutritionDB[key].calories, 0)
+}
+
+const lowCarbs = (obj) => {
+  return filterEntries(obj, ([key, val]) => nutritionDB[key].carbs < 50)
+}
+
+const cartTotal = (obj) => {
+  return mapEntries(obj, ([key, val]) => nutritionDB[key])
+}
