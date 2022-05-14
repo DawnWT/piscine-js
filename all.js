f99aa4cd39f3deec369988bf0bc5c791065f9334
Martin Juillard <martin.juillardfontanille@ynov.com>
2022-05-14 10:00:05 +0200

    fix

--- all.js
+++ all.js
@@ -1,7 +1,7 @@
 const all = async (arr) => {
   let ret = []
   if (typeof arr === 'object' && !Array.isArray(arr)) ret = {}
-  for (const p of arr) ret.push(await p())
+  else for (const p of arr) ret.push(await p())
 
   return ret
 }
