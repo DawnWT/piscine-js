e51da380db064057bc9f08396435786a463191c8
Martin Juillard <martin.juillardfontanille@ynov.com>
2022-05-14 08:55:16 +0200

    fix

--- get-json.js
+++ get-json.js
@@ -1,3 +1,16 @@
+// const fakeFetch = async ({ data, error, ...opts } = {}) => ({
+//   ok: !opts.status,
+//   type: 'basic',
+//   status: 200,
+//   statusText: 'OK',
+//   json: async () => ({ data, error }),
+//   text: async () => JSON.stringify({ data, error }),
+//   ...opts,
+// })
+
+// const error = `oops: ${Math.random()}`
+// fetch = (url) => fakeFetch({ url, error })
+
 const getJSON = async (path, params) => {
   let url = path
   const entries = Object.entries(params)
@@ -8,6 +21,6 @@ const getJSON = async (path, params) => {
   const urlFetch = await fetch(url.slice(0, -1))
   const json = await urlFetch.json()
 
-  if (urlFetch.statusText === 'OK') return json.data
+  if (urlFetch.statusText === 'OK' && json.data) return json.data
   throw new Error(json.error || urlFetch.statusText)
 }
